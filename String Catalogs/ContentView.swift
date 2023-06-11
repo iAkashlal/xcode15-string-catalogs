ee3e709f3cac39db0666ea9a162f8124e5c94179
Akashlal Bathe <tronicxo@gmail.com>
2023-06-11 09:18:11 +0530

    Added available localization variation options
    Variations help supply singular/plural tense, vary by device translations

--- String Catalogs/ContentView.swift
+++ String Catalogs/ContentView.swift
@@ -9,18 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var selection = 1
+    
     var body: some View {
-        List {
-            Text("General", comment: "Show General settings")
-            Text("Control Center", comment: "Show Control Center settings")
-            Text("Display & Brightness", comment: "Show display settings")
-            Text("Home Screen", comment: "Show home screen settings")
-            Text("Accessibility", comment: "Show Accessiblity Settings")
-            Text("Wallpaper", comment: "Show Wallpaper settings")
-            Text("Face ID", comment: "Show Face ID Settings")
-            Text("Emergency SOS", comment: "Show Emergency SOS Settings")
+        
+        VStack(alignment: .leading) {
+            Text("\(selection) Modification", comment: "Begins with 1. The variable selection increases when the list is tapped on")
+            List {
+                Text("General", comment: "Show General settings")
+                Text("Control Center", comment: "Show Control Center settings")
+                Text("Display & Brightness", comment: "Show display settings")
+                Text("Home Screen", comment: "Show home screen settings")
+                Text("Accessibility", comment: "Show Accessiblity Settings")
+                Text("Wallpaper", comment: "Show Wallpaper settings")
+                Text("Face ID", comment: "Show Face ID Settings")
+                Text("Emergency SOS", comment: "Show Emergency SOS Settings")
+            }
+            .onTapGesture {
+                selection += 1
+            }
         }
         .padding()
+        
     }
 }
 
